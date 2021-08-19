5540278e9d49e26273969505487b4d393c4611f3
chazdean <chaz.dean@shopify.com>
2021-08-19 01:02:30 +0000

    removed input function to seperate file

--- play.js
+++ play.js
@@ -1,34 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
-//Handles user input
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
-
-/* const handleUserInput = function() {
-  stdin.on('data', (key) => {
-    console.log("input recvied");
-    if (key === '\u0003') {
-      console.log("exiting");
-      process.exit();
-    }
-  });
-}; */
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    console.log("exiting");
-    process.exit();
-  }
-};
 
 setupInput();
