eca3ab5b88e4ebed3d9838d16f99ddc360e973c6
chazdean <chaz.dean@shopify.com>
2021-08-19 02:09:34 +0000

    added WASD movement conditions

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection; // Stores the active TCP connection object.
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -10,9 +13,22 @@ const setupInput = function() {
 };
 
 const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    console.log("exiting");
+  switch (key) {
+  case '\u0003':
     process.exit();
+    break;
+  case 'w':
+    connection.write("Move: up");
+    break;
+  case 'a':
+    connection.write("Move: left");
+    break;
+  case 's':
+    connection.write("Move: down");
+    break;
+  case 'd':
+    connection.write("Move: right");
+    break;
   }
 };
 
