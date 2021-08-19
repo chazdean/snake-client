9b386f182924237e0a3917970cf0de75b7cf2991
chazdean <chaz.dean@shopify.com>
2021-08-19 03:10:55 +0000

    imported movement and message key constants

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { movementKeys, messages } = require('./constants');
+
 let connection; // Stores the active TCP connection object.
 
 const setupInput = (conn) => {
@@ -13,31 +15,14 @@ const setupInput = (conn) => {
 };
 
 const handleUserInput = (key) => {
-  switch (key) {
-  case '\u0003':
+  if (key === '\u0003') {
     process.exit();
-    break;
-  case 'w':
-    connection.write("Move: up");
-    break;
-  case 'a':
-    connection.write("Move: left");
-    break;
-  case 's':
-    connection.write("Move: down");
-    break;
-  case 'd':
-    connection.write("Move: right");
-    break;
-  case 'z':
-    connection.write("Say: YES!");
-    break;
-  case 'x':
-    connection.write("Say: Too Slow!");
-    break;
-  case 'c':
-    connection.write("Say: Keep Up!");
-    break;
+  }
+  if (movementKeys[key]) {
+    connection.write(movementKeys[key]);
+  }
+  if (messages[key]) {
+    connection.write(messages[key]);
   }
 };
 
