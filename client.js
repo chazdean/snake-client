6f57da4caead9e7943f25886937742e2c36dd19a
chazdean <chaz.dean@shopify.com>
2021-08-19 03:52:57 +0000

    corrected indentation and spacing

--- client.js
+++ client.js
@@ -1,27 +1,20 @@
 const net = require('net');
 const { IP, PORT } = require('./constants');
-/**
- * Establishes connection with the game server
- */
+
 const connect = function() {
   const conn = net.createConnection({
     host: IP,
     port: PORT
   });
- 
   conn.setEncoding('utf8');
-  
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
-
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write('Name: CHZ');
   });
- 
   return conn;
 };
-
 
 module.exports = { connect };
